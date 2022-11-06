52c9973ee99109e03304a9f57fba5c0e7ab83572
pecially520 <62271580+pecially520@users.noreply.github.com>
2022-11-06 23:11:39 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -115,7 +115,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://v.5ms.cc/box/v.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
